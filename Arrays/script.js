06faa97157ce1631be54554bc8b46ac18efb9a25
Huzaifa Warsi <huzaifawarsi2006@gmail.com>
2025-11-13 16:15:30 +0500

    Javascript-Assignments

---
+++ Arrays/script.js
@@ -0,0 +1,161 @@
+
+//Arrays:
+
+
+// var students = ["ahmed","ali","ibrahim","zain","fahad"]
+
+// students[5] = "Qasim"
+// console.log(students[5]); 
+
+
+// var arr = []
+// arr[0] = "A"
+// arr[1] = "B"
+  
+// console.log(arr);
+
+
+// let heroes = ["Abdul Qadeer Khan", "Fatima Jinnah", "Abdul Sattar Edhi","Allama Iqbal", "Quaid-e-azam"];
+
+// for (let i = 0; i<heroes.length; i++){
+//     console.log(heroes[i]);
+// }
+
+// for (let hero of heroes){
+//     console.log(hero.toUpperCase());
+// }
+
+
+
+// let marks = [85, 97, 44, 37, 76, 60];
+// let sum = 0;
+
+// for (let mark of marks){
+//     sum = sum + mark;
+// }
+
+// let avg = sum / marks.length;
+// console.log(avg);
+
+// let heroes = ["Abdul Qadeer Khan", "Fatima Jinnah", "Abdul Sattar Edhi","Allama Iqbal", "Quaid-e-azam"];
+  
+//  for(let value of heroes){
+//     console.log(value);
+//  }
+
+
+// let marks = [85, 97, 44, 37, 76, 60];
+
+// let sum = 0;
+
+// for(let val of marks){
+    
+//     sum = sum + val;
+
+// }
+
+// let avg = sum / marks.length;
+
+// console.log(avg);
+
+
+
+// let items = [250, 645, 300, 900, 50];
+
+// let i = 0;
+// for(let val of items){
+    
+//     console.log(`value of index ${i} = ${val}`);
+//     let offer = val / 10;
+//     items[i] = items[i] - offer;
+//     console.log(`value after offer =${items[i]}`)
+//     i++;
+
+    
+// }
+
+
+// Offer Programm 
+
+// let  items = [250, 645, 300, 900, 50];
+// let i = 0;
+// for(let val of items){
+//     console.log(`value of index ${i} =${val}`);
+//     let offer = val / 15;
+//     items[i] = items[i] - offer;
+//     console.log(`value after offer =${items[i]}`);
+    
+
+//     i++;
+// } 
+
+
+
+
+
+
+
+
+
+
+// let vegitables = ["Carrots", "broccoli" ,"Parrots", "Croccoli"];
+
+// console.log(vegitables);
+
+// console.log(vegitables.slice(0,1));
+
+
+
+// let items = fruits.concat(vegitables);
+// console.log(items);
+
+// let arr = [1,2,3,4,5,6,7,8,9];
+
+ 
+// console.log(arr);
+
+
+
+
+
+// let arr = ["Bloomberg", "Microsoft", "Uber", "Google", "IBM", "Netflix"];
+
+// arr.shift();
+// arr.splice(1 ,1,"Ola")
+// arr.push("Amazon");
+// console.log(arr);
+
+
+
+
+
+
+
+
+// console.log(vegitables.toString());
+
+
+// let deletedItem = vegitables.pop();
+// console.log(deletedItem);
+
+
+
+
+// let companies = ["Bloomberg", "Microsoft", "Uber", "Google", "IBM", "Netflix"];
+
+// Add Element in the end
+// companies.push("IDC");
+
+// Remove Element from the end
+// companies.pop();
+
+// Remove from Start
+// companies.shift();
+
+// Add an element in the start
+// companies.unshift("OLP");
+
+let arr = [1,2,3,4,5,6,7,8,9];
+
+console.log(arr.splice(1,6,3,4));
+
