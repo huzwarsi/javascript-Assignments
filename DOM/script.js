836b09b4f5e8ffa2c8df719c261cf802e02835c2
Huzaifa Warsi <huzaifawarsi2006@gmail.com>
2025-11-16 15:17:25 +0500

    updated

---
+++ DOM/script.js
@@ -0,0 +1,66 @@
+
+// let heading =  document.getElementById("heading");
+// console.dir(heading)
+
+// let headings = document.getElementsByClassName("headings");
+
+// console.dir(headings);
+
+// let paras = document.getElementsByTagName("p");
+// console.log(paras);
+
+// let div = document.querySelector("div");
+// console.dir(div);
+
+// let h1 = document.querySelector("h1");
+// console.dir(h1.innerText);
+
+// h1.innerText = h1.innerText + " From Apna College"
+
+// let divs = document.querySelectorAll(".box");
+
+// i = 1;
+// for(div of divs){
+//     div.innerText = `New Value ${i}`
+//     i++;
+// }
+
+// divs[0].innerText = "New Unique 0";
+// divs[1].innerText = "New Unique 1";
+// divs[2].innerText = "New Unique 2";
+
+
+// let div = document.getElementsByClassName(".box");
+// console.log(div.innerText);
+
+// let h1 = document.getElementsByClassName(".head");
+// console.log(h1.innerText);
+
+// let h1 = document.getElementById("#head");
+// console.log(h1);
+
+// let head = document.getElementsByTagName("h1");
+// console.dir(head.innerHTML);
+
+// let h1 = document.querySelector(".head");
+// console.log(h1.innerText);
+
+// let text = document.getElementById("para");
+// console.log(text.innerText)
+
+
+// let btn = document.createElement("button");
+// btn.innerText = "Click me"
+
+
+
+// btn.style.backgroundColor = "red";
+// btn.style.color = "black";
+
+// document.querySelector("body").prepend(btn);
+
+
+
+for(let i = 1; i<=1000; i++){
+    console.log("Hello JS!");
+}
